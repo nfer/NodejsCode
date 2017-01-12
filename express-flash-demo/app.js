ca4eab29378c6b50e51638497f187585c372e443
Nfer Zhuang <nfer.zhuang@gmail.com>
2017-01-12 20:57:35 +0800

    add bootstrap css

--- express-flash-demo/app.js
+++ express-flash-demo/app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 app.get('/', (req, res) => {
   res.render('index');
